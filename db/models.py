9047fa1ecc3a601485cc676f8dd36de3acd64d16
dimazzzik <79855381+dimazzz1k@users.noreply.github.com>
2022-10-19 22:43:48 +0300

    db update
    переделал всю бд

--- db/models.py
+++ db/models.py
@@ -1,79 +1,104 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, SmallInteger, Numeric
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey, SmallInteger, Numeric, DATE
+from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
 
 
-class Students(Base):
-    __tablename__ = "students"
-    
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(16), nullable=False)
-    last_name = Column(String(16), nullable=False)
-    login = Column(String(32), nullable=False)
-    password = Column(String(32), nullable=False)
-    
-
-class Books(Base):
+class Book(Base):
     __tablename__ = "books"
     
-    id = Column(Integer, primary_key=True)
-    type_id = Column(SmallInteger, ForeignKey("book_types.id"), nullable=False)
-    title = Column(String(64), nullable=False)
-    author = Column(String(64), nullable=False)
-    series = Column(String(64))
-    genre_id = Column(Integer, ForeignKey("genres.id"))
-    page_total = Column(Integer, nullable=False)
-    publisher_id = Column(Integer, ForeignKey("publishers.id"), nullable=False)
-    publication_year = Column(String(4), nullable=False)
-    department_id = Column(Integer, ForeignKey("departments.id"), nullable=False)
-    receipt_year = Column(String(4), nullable=False)
-    price = Column(Numeric(19, 4))
-    isbn = Column(String(17))
-    bbk = Column(String(32))
-    decommision_id = Column(Integer, ForeignKey("decommisions.id"))
+    id: int = Column(Integer, primary_key=True)
+    type_id: int = Column(SmallInteger, ForeignKey("book_types.id"), nullable=False)
+    title: str = Column(String(64), nullable=False)
+    author: str = Column(String(64), nullable=False)
+    series: str = Column(String(64))
+    genre_id: int = Column(Integer, ForeignKey("book_genres.id"))
+    page_total: int = Column(Integer, nullable=False)
+    publisher_id: int = Column(Integer, ForeignKey("publishers.id"), nullable=False)
+    publication_year: str = Column(String(4), nullable=False)
+    department_id: int = Column(Integer, ForeignKey("departments.id"), nullable=False)
+    receipt_year: str = Column(String(4), nullable=False)
+    price: float = Column(Numeric(19, 4))
+    isbn: str = Column(String(17))
+    bbk: str = Column(String(32))
+    decommision_id: int = Column(Integer, ForeignKey("book_decommisions.id"))
+    student_id: int = Column(Integer, ForeignKey("students.id"), nullable=False)
+    state_id: int = Column(SmallInteger, ForeignKey("book_states.id"), nullable=False)
+    receive_date: str = Column(DATE)
+    return_date: str = Column(DATE)
     
-    
-class UsageHistory(Base):
-    __tablename__ = "usage_history"
-    
-    id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey("students.id"), nullable=False)
-    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
-    
+    type: "BookType" = relationship("BookType", back_populates="books")
+    genre: "BookGenre" = relationship("BookGenre", back_populates="books")
+    publisher: "Publisher" = relationship("Publisher", back_populates="books")
+    department: "Department" = relationship("Department", back_populates="books")
+    decommision: "BookDecommision" = relationship("BookDecommision", back_populates="books")
+    student: "Student" = relationship("Student", back_populates="books")
+    state: "BookState" = relationship("BookState", back_populates="books")
 
-class BookTypes(Base):
+
+class BookType(Base):
     __tablename__ = "book_types"
     
-    id = Column(SmallInteger, primary_key=True)
-    name = Column(String(32), nullable=False)
+    id: int = Column(SmallInteger, primary_key=True)
+    name: str = Column(String(32), nullable=False)
     
+    books: "Book" = relationship("Book", back_populates="type")
 
-class Decommisions(Base):
-    __tablename__ = "decommisions"
-    
-    id = Column(Integer, primary_key=True)
-    books_total = Column(Integer, nullable=False)
-    
 
-class Departments(Base):
-    __tablename__ = "departments"
+class BookGenre(Base):
+    __tablename__ = "book_genres"
     
-    id = Column(Integer, primary_key=True)
-    title = Column(String(32), nullable=False)
+    id: int = Column(Integer, primary_key=True)
+    title: str = Column(String(32), nullable=False)
     
+    books: "Book" = relationship("Book", back_populates="genre")
 
-class Genres(Base):
-    __tablename__ = "genres"
-    
-    id = Column(Integer, primary_key=True)
-    title = Column(String(32), nullable=False)
-    
-    
-class Publishers(Base):
+
+class Publisher(Base):
     __tablename__ = "publishers"
     
-    id = Column(Integer, primary_key=True)
-    title = Column(String(64), nullable=False)
-    city = Column(String(16), nullable=False)
+    id: int = Column(Integer, primary_key=True)
+    name: str = Column(String(64), nullable=False)
+    city: str = Column(String(16), nullable=False)
+    
+    books: "Book" = relationship("Book", back_populates="publisher")
+    
+
+class Department(Base):
+    __tablename__ = "departments"
+    
+    id: int = Column(Integer, primary_key=True)
+    name: str = Column(String(32), nullable=False)
+    
+    books: "Book" = relationship("Book", back_populates="department")
+
+
+class BookDecommision(Base):
+    __tablename__ = "book_decommisions"
+    
+    id: int = Column(Integer, primary_key=True)
+    books_total: int = Column(Integer, nullable=False)
+    
+    books: "Book" = relationship("Book", back_populates="decommision")
+
+
+class Student(Base):
+    __tablename__ = "students"
+    
+    id: int = Column(Integer, primary_key=True)
+    first_name: str = Column(String(32), nullable=False)
+    last_name: str = Column(String(32), nullable=False)
+    login: str = Column(String(32), nullable=False)
+    password: str = Column(String(32), nullable=False)
+    
+    books: "Book" = relationship("Book", back_populates="student")
+
+  
+class BookState(Base):
+    __tablename__ = "book_states"
+    
+    id: int = Column(SmallInteger, primary_key=True)
+    name: str = Column(String(32), nullable=False)
+    
+    books: "Book" = relationship("Book", back_populates="state")

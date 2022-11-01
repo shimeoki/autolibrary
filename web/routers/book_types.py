07036b5e12275b6cba6d31be9c2075a6b024567e
dimazzzik <79855381+dimazzz1k@users.noreply.github.com>
2022-11-01 23:02:23 +0300

    Update book_types.py

--- web/routers/book_types.py
+++ web/routers/book_types.py
@@ -8,15 +8,20 @@ from db.schemas import BookType, BookTypeCreate
 router = APIRouter()
 
 
-@router.post("/", response_model=BookType)
+@router.post("/", response_model=BookType, status_code=201)
 def create_book_type(book_type: BookTypeCreate, book_type_repo: Session = Depends(get_book_type_repo)):
     db_book_type = book_type_repo.create(book_type)
     
     return db_book_type
 
 
-@router.get("/{book_type_id}", response_model=BookType)
+@router.get("/{book_type_id}", response_model=BookType, status_code=200)
 def read_book_type(book_type_id: int, book_type_repo: Session = Depends(get_book_type_repo)):
     db_book_type = book_type_repo.read(book_type_id)
     # проверка на отсутствие
     return db_book_type
+
+
+@router.delete("/{book_type_id}", status_code=200)
+def delete_book_type(book_type_id: int, book_type_repo: Session = Depends(get_book_type_repo)):
+    book_type_repo.delete(book_type_id)
